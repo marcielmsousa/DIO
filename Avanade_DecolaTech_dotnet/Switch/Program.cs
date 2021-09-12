ed26faf9275f0f09bdfaea9b295ded679117977e
Marciel M Sousa <marcielmsousa@gmail.com>
2021-09-12 13:17:56 -0300

    Commit inicial

---
+++ Avanade_DecolaTech_dotnet/Switch/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = args.Length;
+            
+            switch (num)
+            {
+                case 0:
+                    log("0 argumentos");
+                    break;
+                case 1:
+                    log("caso 1");
+                    break;
+                case 2:
+                    log("caso 2");
+                    break;
+                default:
+                    log("default");
+                    log($"{num} argumentos");
+                    break;
+            }
+        }
+
+        private static void log(String line){
+            Console.WriteLine(line);
+        }
+    }
+}
