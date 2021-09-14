4f22917a8214ec2ef2aeb870117e14e312cbaef5
Marciel M Sousa <marcielmsousa@gmail.com>
2021-09-14 11:40:32 -0300

    Codigo base desafio concluido

---
+++ Avanade_DecolaTech_dotnet/DIO.Series/Classes/SerieRepositorio.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using DIO.Series.Interfaces;
+
+namespace DIO.Series
+{
+    public class SerieRepositorio : IRepositorio<Serie>
+    {
+
+        public List<Serie> listaSerie = new List<Serie>();
+
+        public List<Serie> Lista()
+        {
+            return listaSerie;
+        }
+
+        public Serie RetornaPorId(int id)
+        {
+            return listaSerie[id];
+        }
+
+        public void Insere(Serie objeto)
+        {
+            listaSerie.Add(objeto); 
+        }
+
+        public void Exclui(int id)
+        {
+            listaSerie[id].Excluir();
+        }
+
+        public void Atualiza(int id, Serie objeto)
+        {
+            listaSerie[id] = objeto;
+        }
+
+        public int ProximoId()
+        {
+            return listaSerie.Count;
+        }
+    }
+}
