ac6fead641933e48758b343b5446f6e6b1c5d07b
Marciel M Sousa <marcielmsousa@gmail.com>
2021-09-14 18:47:22 -0300

    Implementação de exemplo simples para requisição SOAP

---
+++ Avanade_DecolaTech_dotnet/WebServices/SOAP_REST/SERVICES/soap.py
@@ -0,0 +1,7 @@
+﻿from zeep import Client
+
+client = Client('http://soapclient.com/xml/soapresponder.wsdl')
+#result = client.service.Method1(bstrParam1='oi', bstrParam2='tchau')
+result = client.service.Method1(bstrParam2='tchau', bstrParam1='oi')
+#result = client.service.Method1('tchau', 'oi')
+print(result)
